98a0d13b2cf25d7baf0312fa63c676039f001029
Simon Laing <laingsimon@users.noreply.github.com>
2022-04-11 19:02:13 +0100

    Update IAccessService.cs

--- HolcombeScores.Api/Services/IAccessService.cs
+++ HolcombeScores.Api/Services/IAccessService.cs
@@ -19,6 +19,6 @@ namespace HolcombeScores.Api.Services
         Task<ActionResultDto<AccessDto>> RevokeAccess(AccessResponseDto accessResponseDto);
         IAsyncEnumerable<AccessDto> GetAllAccess();
         IAsyncEnumerable<RecoverAccessDto> GetAccessForRecovery();
-        Task<ActionResultDto<AccessDto>> RecoverAccess(Access access);
+        Task<ActionResultDto<AccessDto>> RecoverAccess(RecoverAccessDto recoverAccessDto, string adminPassCode);
     }
 }
