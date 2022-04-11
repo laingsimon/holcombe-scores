db2961ba8fbb062d1a5385e5b6580897881ed540
Simon Laing <laingsimon@users.noreply.github.com>
2022-04-11 12:17:36 +0100

    Update and rename IAccessRecoveryDtoAdapter.cs to IRecoverAccessDtoAdapter.cs

---
+++ HolcombeScores.Api/Services/Adapters/IRecoverAccessDtoAdapter.cs
@@ -0,0 +1,10 @@
+using HolcombeScores.Api.Models;
+using HolcombeScores.Models;
+
+namespace HolcombeScores.Api.Services.Adapters
+{
+    public interface IRecoverAccessDtoAdapter
+    {
+        RecoverAccessDto Adapt(Access access);
+    }
+}
