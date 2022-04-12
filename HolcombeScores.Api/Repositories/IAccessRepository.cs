e3e6683f977be2af6b83bef3d1db9f0a2ffec0d7
Simon Laing <laingsimon@users.noreply.github.com>
2022-04-12 16:14:55 +0100

    Add userId access retrieval

--- HolcombeScores.Api/Repositories/IAccessRepository.cs
+++ HolcombeScores.Api/Repositories/IAccessRepository.cs
@@ -11,6 +11,7 @@ namespace HolcombeScores.Api.Repositories
         IAsyncEnumerable<AccessRequest> GetAllAccessRequests();
         Task<AccessRequest> GetAccessRequest(string token);
         Task<Access> GetAccess(string token);
+        Task<Access> GetAccess(Guid userId);
         Task AddAccessRequest(AccessRequest accessRequest);
         Task AddAccess(Access access);
         Task RemoveAccessRequest(Guid userId);
