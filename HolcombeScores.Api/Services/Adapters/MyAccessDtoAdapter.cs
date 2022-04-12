9ce03012c030fa87bb0fa51edb5bad97e7337507
Simon Laing <laingsimon@users.noreply.github.com>
2022-04-12 16:36:05 +0100

    Create MyAccessDtoAdapter.cs

---
+++ HolcombeScores.Api/Services/Adapters/MyAccessDtoAdapter.cs
@@ -0,0 +1,26 @@
+using HolcombeScores.Api.Models;
+using HolcombeScores.Models;
+
+namespace HolcombeScores.Api.Services.Adapters
+{
+    public class MyAccessDtoAdapter : IMyAccessDtoAdapter
+    {
+        private readonly IAccessDtoAdapter _accessDtoAdapter;
+        private readonly IAccessRequestDtoAdapter _accessRequestDtoAdapter;
+
+        public MyAccessDto adapter(IAccessDtoAdapter accessDtoAdapter, IAccessRequestDtoAdapter accessRequestDtoAdapter)
+        {
+            _accessDtoAdapter = accessDtoAdapter;
+            _accessRequestDtoAdapter = accessRequestDtoAdapter;
+        }
+
+        public MyAccessDto Adapt(Access access, AccessRequest accessRequest)
+        {
+            return new MyAccessDto
+            {
+                Access = _accessDtoAdapter.Adapt(access),
+                AccessRequest = _accessRequestDtoAdapter.Adapt(accessRequest),
+            };
+        }
+    }
+}
