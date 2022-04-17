1d7d610a29ce1a0c4573b7dbee46c3cc2b0a46de
Simon Laing <laingsimon@users.noreply.github.com>
2022-04-17 21:02:07 +0100

    Update NewGameDtoAdapter.cs

--- HolcombeScores.Api/Services/Adapters/NewGameDtoAdapter.cs
+++ HolcombeScores.Api/Services/Adapters/NewGameDtoAdapter.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using HolcombeScores.Api.Models;
 using HolcombeScores.Api.Repositories;
 using HolcombeScores.Models;
