f0d997fec82bae71d6efcf1fafceb177cda9d930
Simon Laing <laingsimon@users.noreply.github.com>
2022-04-17 20:48:57 +0100

    Update Goal.cs

--- HolcombeScores.Models/Goal.cs
+++ HolcombeScores.Models/Goal.cs
@@ -1,13 +1,27 @@
 using System;
+using Azure;
+using Azure.Data.Tables;
 
 namespace HolcombeScores.Models
 {
-    public class Goal
+    public class Goal : ITableEntity
     {
         public DateTime Time { get; set; }
         public bool HolcombeGoal { get; set; }
         public int PlayerNumber { get; set; }
         public Guid TeamId { get; set; }
         public Guid GameId { get; set; }
+
+        /// <summary>
+        /// GameId
+        /// </summary>
+        public string PartitionKey { get; set; }
+
+        /// <summary>
+        /// GoalId
+        /// </summary>
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
     }
 }
