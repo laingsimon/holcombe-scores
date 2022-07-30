8c35c6d65e5c1dbc66596dcb5915eaa2c829ca6f
Simon Laing <laingster@gmail.com>
2022-07-30 14:30:55 +0100

    #42: Get user access at top level component and pass down to children

--- HolcombeScores.Api/ClientApp/src/components/Layout.js
+++ HolcombeScores.Api/ClientApp/src/components/Layout.js
@@ -1,16 +1,45 @@
-import React, { Component } from 'react';
-import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
+import React, {Component} from 'react';
+import {Container} from 'reactstrap';
+import {NavMenu} from './NavMenu';
+import {Http} from "../api/http";
+import {Access} from "../api/access";
+import {Settings} from "../api/settings";
 
 export class Layout extends Component {
-  render () {
-    return (
-      <div>
-        <NavMenu />
-        <Container>
-          {this.props.children}
-        </Container>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        const http = new Http(new Settings());
+        this.accessApi = new Access(http);
+        this.state = {
+            loading: true,
+            access: null,
+        };
+    }
+
+    async componentDidMount() {
+        const access = await this.accessApi.getMyAccess();
+        this.setState({
+            access: access.access,
+            loading: false
+        });
+    }
+
+    render() {
+        if (this.loading) {
+            return (<div className="d-flex justify-content-center">
+                <div className="spinner-border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            </div>);
+        }
+
+        return (
+            <div>
+                <NavMenu access={this.state.access}/>
+                <Container>
+                    {this.props.children}
+                </Container>
+            </div>
+        );
+    }
 }
