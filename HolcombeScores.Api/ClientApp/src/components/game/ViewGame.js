814c7c2d0b50b064d78122f0da92d958626aea4a
Simon Laing <laingsimon@users.noreply.github.com>
2022-08-05 07:38:13 +0100

    Remove superfluous import

--- HolcombeScores.Api/ClientApp/src/components/game/ViewGame.js
+++ HolcombeScores.Api/ClientApp/src/components/game/ViewGame.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {Functions} from '../../functions'
 import {GoalOverview} from './GoalOverview';
-import { Link } from 'react-router-dom';
 
 /*
 * Props:
