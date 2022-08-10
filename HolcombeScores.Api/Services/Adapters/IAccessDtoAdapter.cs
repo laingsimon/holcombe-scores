82fe4a44ab2acb7998a9c4cc993e174f8aaae897
Simon Laing <laingsimon@users.noreply.github.com>
2022-08-10 17:58:14 +0100

    Update IAccessDtoAdapter.cs

--- HolcombeScores.Api/Services/Adapters/IAccessDtoAdapter.cs
+++ HolcombeScores.Api/Services/Adapters/IAccessDtoAdapter.cs
@@ -6,6 +6,6 @@ namespace HolcombeScores.Api.Services.Adapters
     public interface IAccessDtoAdapter
     {
         Access Adapt(AccessDto access);
-        AccessDto Adapt(Access access);
+        AccessDto Adapt(Access access, Access impersonatedBy = null);
     }
 }
