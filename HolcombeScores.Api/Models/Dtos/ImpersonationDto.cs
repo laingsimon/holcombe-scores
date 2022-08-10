1c9c01f348e83a21819c56066ff66ed5984bb1e2
Simon Laing <laingsimon@users.noreply.github.com>
2022-08-10 17:52:14 +0100

    Create ImpersonationDto.cs

---
+++ HolcombeScores.Api/Models/Dtos/ImpersonationDto.cs
@@ -0,0 +1,11 @@
+namespace HolcombeScores.Api.Models.Dtos
+{
+    /// <summary>
+    /// A representation of an impersonation of another user
+    /// </summary>
+    public class ImpersonationDto
+    {
+        public Guid UserId { get; set; }
+        public string AdminPassword { get; set; }
+    }
+}
