73f027dc6150021a3c8b77040608bb324255bb38
Simon Laing <laingster@gmail.com>
2022-09-11 09:08:51 +0100

    #71: Add ability to see and end all testing contexts from the UI

--- HolcombeScores.Api/ClientApp/src/components/admin/TestingAdmin.js
+++ HolcombeScores.Api/ClientApp/src/components/admin/TestingAdmin.js
@@ -17,18 +17,27 @@ export class TestingAdmin extends Component {
         this.testingApi = new Testing(new Http(new Settings()));
         this.startTesting = this.startTesting.bind(this);
         this.endTesting = this.endTesting.bind(this);
+        this.endAllTestingContexts = this.endAllTestingContexts.bind(this);
         this.state = {
-            loading: false
+            starting: false,
+            ending: false,
+            endingAllContexts: false,
+            loadingAllContexts: false,
+            allContexts: [],
         }
     }
 
+    async componentDidMount() {
+        await this.reloadAllContexts();
+    }
+
     async startTesting() {
-        if (this.state.loading) {
+        if (this.state.starting || this.state.ending || this.state.endingAllContexts) {
             return;
         }
 
         this.setState({
-            loading: true
+            starting: true
         });
 
         try {
@@ -38,62 +47,126 @@ export class TestingAdmin extends Component {
             });
 
             await this.props.reloadAll();
+            await this.reloadAllContexts();
 
             this.setState({
-                loading: false
+                starting: false
             });
         } catch (e) {
             this.setState({
                 error: e,
-                loading: false
+                starting: false
             });
         }
     }
 
     async endTesting() {
-        if (this.state.loading) {
+        if (this.state.starting || this.state.ending || this.state.endingAllContexts) {
             return;
         }
 
         this.setState({
-            loading: true
+            ending: true
         });
 
         try {
             await this.testingApi.stopTesting();
 
             await this.props.reloadAll();
+            await this.reloadAllContexts();
 
             this.setState({
-                loading: false
+                ending: false
             });
         } catch (e) {
             this.setState({
                 error: e,
-                loading: false
+                ending: false
             });
         }
     }
 
+    async endAllTestingContexts() {
+        if (this.state.starting || this.state.ending || this.state.endingAllContexts) {
+            return;
+        }
+
+        this.setState({
+            endingAllContexts: true
+        });
+
+        try {
+            await this.testingApi.endAllTestingContexts();
+
+            await this.props.reloadAll();
+            await this.reloadAllContexts();
+
+            this.setState({
+                endingAllContexts: false
+            });
+        } catch (e) {
+            this.setState({
+                error: e,
+                endingAllContexts: false
+            });
+        }
+    }
+
+    // renderers
     render() {
         return (<div>
             <p>Status: <b>{this.props.testing ? `Testing data in use` : 'Production data in use'}</b></p>
             {this.state.error ? (<Alert errors={this.state.error} />) : null}
             {this.props.testing ? this.renderContextOptions() : null}
-            <div>
-                {this.props.testing ? null : (<button className="btn btn-warning margin-right" onClick={this.startTesting}>
-                    {this.state.loading ? (<span className="spinner-border spinner-border-sm margin-right" role="status" aria-hidden="true"></span>) : null}
-                    Start testing</button>)}
-            </div>
+            {this.props.testing ? null : (<button className="btn btn-warning margin-right" onClick={this.startTesting}>
+                {this.state.starting ? (<span className="spinner-border spinner-border-sm margin-right" role="status" aria-hidden="true"></span>) : null}
+                Start testing</button>)}
+            {this.props.testing ? (<button className="btn btn-secondary margin-right" onClick={this.endTesting}>
+                {this.state.ending ? (<span className="spinner-border spinner-border-sm margin-right" role="status" aria-hidden="true"></span>) : null}
+                End testing</button>) : null}
+            <button className="btn btn-danger margin-right" onClick={this.endAllTestingContexts}>
+                {this.state.endingAllContexts ? (<span className="spinner-border spinner-border-sm margin-right" role="status" aria-hidden="true"></span>) : null}
+                End all contexts</button>
+            {this.renderAllContexts()}
         </div>)
     }
 
     renderContextOptions() {
         return (<div>
             <p>Context: <b>{this.props.testing}</b></p>
-            <button className="btn btn-secondary" onClick={this.endTesting}>
-                {this.state.loading ? (<span className="spinner-border spinner-border-sm margin-right" role="status" aria-hidden="true"></span>) : null}
-                End testing</button>
         </div>);
+    }
+
+    renderAllContexts() {
+        return (<div>
+            <hr />
+            <h4>
+                {this.state.loadingAllContexts ? (<span className="spinner-border spinner-border-sm margin-right" role="status" aria-hidden="true"></span>) : null}
+                All contexts : {this.state.allContexts.length}</h4>
+            <ul>
+                {this.state.allContexts.map(context => (<li key={context.contextId}>{context.contextId}: {context.tables} table/s {context.self ? <i> - This testing context</i> : null}</li>))}
+            </ul>
+        </div>)
+    }
+
+    //api functions
+    async reloadAllContexts() {
+        this.setState({
+            loadingAllContexts: true
+        });
+
+        try {
+            const contexts = await this.testingApi.getAllTestingContexts();
+
+            this.setState({
+                loadingAllContexts: false,
+                allContexts: contexts
+            });
+        } catch (e) {
+            this.setState({
+                loadingAllContexts: false,
+                error: e
+            });
+        }
     }
 }
